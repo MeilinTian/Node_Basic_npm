950e651ea679081832e62c72167116f809cea870
meilin <fanny_tianmeilin@163.com>
2023-01-29 14:11:43 +0800

    self defined module

---
+++ 4-1-module.js
@@ -0,0 +1,13 @@
+// self-defined module
+module.exports.username = 'Lily'
+
+module.exports.sayHello = function() {
+    console.log('Hello.')
+}
+
+module.exports = {
+    nickname: 'Irene',
+    sayHi() {
+        console.log('Hi.')
+    }
+}
---
+++ 4-2-test.js
@@ -0,0 +1,3 @@
+// test self-defined module
+const m = require('./4-1-module.js')
+console.log(m)
